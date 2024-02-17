42338d500beceed13df13e376389fc4bd0e6cdc7
josephnady <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@github.com>
2024-02-16 18:41:53 -0800

    advanced tasks

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def my_print():
+    """function that use sys write to print"""
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
+    exit(1)
+my_print()
+
