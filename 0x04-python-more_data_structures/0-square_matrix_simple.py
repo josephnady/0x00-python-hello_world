dbdb317a1fb83b4953df8f12811dd01b58e564b8
josephnady <jozephnady#gmail.com>
2024-04-15 07:41:16 -0700

    first task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
-
-
+#!/bin/usr/python3
+"""DOC"""
 def square_matrix_simple(matrix=[]):
-    """
-    wordA function that computes the square
-    value of all integers of a matrix.
-    """
-    new_matrix = []
-    for col in matrix:
-        result = list(map(lambda x: x**2, col))
-        new_matrix.append(result)
-    return new_matrix
+    mat = []
+    for i in matrix:
+        row = []
+        for x in i:
+            row.append(x**2)
+        mat.append(row)
+    return mat
+
