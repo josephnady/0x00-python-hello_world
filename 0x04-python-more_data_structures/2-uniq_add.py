af769d1d9873e24d3e583d7904a827557aeb6c6d
josephnady <jozephnady#gmail.com>
2024-04-15 09:09:17 -0700

    adjust pycodestyle

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,6 +5,6 @@
 def uniq_add(my_list=[]):
     """DOC"""
     uniq_lst = set()
-    [list(map(lambda x: uniq_lst.add(x),my_list))]
+    [list(map(lambda x: uniq_lst.add(x), my_list))]
     res = sum(x for x in uniq_lst)
     return (res)
