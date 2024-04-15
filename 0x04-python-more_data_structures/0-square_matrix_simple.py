d8bfd120b323a87703dd7bed8f7c33473e74d840
joseph nady <jozephnady@gmail.com>
2024-04-15 17:20:36 +0200

    solving conflict

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,19 +3,6 @@
 
 
 def square_matrix_simple(matrix=[]):
-<<<<<<< HEAD
-    """DOC"""
-   #mat = []
-   #for i in matrix:
-   #    row = []
-   #    for x in i:
-   #        row.append(x**2)
-   #    mat.append(row)
-   #return mat
-    mat = matrix.apply(map(Lambda x**2: x))
-    return mat
-=======
     """DOCs"""
     mat = [list(map(lambda x: x ** 2, sublist)) for sublist in matrix]
     return mat
->>>>>>> dad0df2209a5241a5fbcd24222e298ec00d8fc5e
