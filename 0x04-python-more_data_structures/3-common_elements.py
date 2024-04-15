393998746e1a7c6a5173c6f5440d9cb0a5730dfd
josephnady <jozephnady#gmail.com>
2024-04-15 09:21:32 -0700

    pycodestyle task4

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -4,7 +4,6 @@
 
 def common_elements(set_1, set_2):
     """DOC"""
-    # return [x if x.isin([y for y in set_2]) for x in set_1]
     res = set()
     [res.add(x) for x in set_1 if x in set_2]
     return (res)
