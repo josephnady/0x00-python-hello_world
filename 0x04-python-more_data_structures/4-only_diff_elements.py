026d6e0efdab887defa78763152fd791cb4925c1
josephnady <jozephnady#gmail.com>
2024-04-15 09:28:18 -0700

    correction for task5

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,4 +6,5 @@ def only_diff_elements(set_1, set_2):
     """DOC"""
     res = set()
     [res.add(x) for x in set_1 if x not in set_2]
+    [res.add(y) for y in set_2 if y not in set_1]
     return (res)
