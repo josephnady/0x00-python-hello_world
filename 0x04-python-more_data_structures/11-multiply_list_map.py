e5a04f0fdc7559554e99394849eb8bc619364fcf
josephnady <jozephnady#gmail.com>
2024-04-15 15:38:36 -0700

    pycodestyle task12

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -4,4 +4,5 @@
 
 def multiply_list_map(my_list=[], number=0):
     """DOC"""
-    return list(map(lambda x: x*number, my_list))
+    new_list = list(map(lambda x: x*number, my_list))
+    return new_list
