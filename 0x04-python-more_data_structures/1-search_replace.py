e3254e7bfe3eed935bebfeacc5d45742dd6b86af
josephnady <jozephnady#gmail.com>
2024-04-15 08:50:17 -0700

    make exe

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """DOC"""
 
+
 def search_replace(my_list, search, replace):
     """DOC"""
-    list2 = [replace if x == search else x for x in my_list ]
+    list2 = [replace if x == search else x for x in my_list]
     return (list2)
