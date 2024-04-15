91483cedc9476346ba8387f03d77065e4a9ab068
josephnady <jozephnady#gmail.com>
2024-04-15 15:25:56 -0700

    pycodestyle task11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,7 +6,7 @@ def best_score(a_dictionary):
     """DOC"""
     if a_dictionary:
         best_score = max(a_dictionary.values())
-        best_achiever = [k for k, v in a_dictionary.items() if v == best_score][0]
-        return best_achiever
+        best_achiever = [k for k, v in a_dictionary.items() if v == best_score]
+        return best_achiever[0]
     else:
         return None
