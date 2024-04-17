0110fa0dc378042229066d60c10b391ac1e751bb
joseph nady <jozephnady@gmail.com>
2024-04-18 00:34:44 +0200

    task 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    yy = list(map(lambda lst: list(map(lambda x: x*x, lst)),matrix))
-    return  yy
+    yy = list(map(lambda lst: list(map(lambda x: x*x, lst)), matrix))
+    return yy
