05260f2f62ca4b6389b10f9c23c3e9992f819152
joseph nady <jozephnady@gmail.com>
2024-04-17 14:14:13 +0200

    task 13 sol 7

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,17 +2,16 @@
 def roman_to_int(roman_string):
     """
     X = 10 / VII = 7  / IX = 9 /
-    LXXXVII = 87 / DCCVII = 707
+    LXXXVII = 87 / DCCVII = 707 /XCIX = 99
     """
-    roman_n = {"I": 1, "V": 5, "X": 10,
-    "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_n = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
     if not isinstance(roman_string, str):
-        return None
+        return 0
 
     for each in roman_string:
         if each not in list(roman_n.keys()):
-            return None
+            return 0
 
     res = [roman_n.get(i) for i in roman_string]
     i = 0
@@ -24,5 +23,10 @@ def roman_to_int(roman_string):
                 if x == 1:
                     res2.remove(x)
                     res2.append(-1)
+        if (i == 100):
+            for x in reversed(res2):
+                if x == 10:
+                    res2.remove(x)
+                    res2.append(-10)
 
     return sum(res2)
