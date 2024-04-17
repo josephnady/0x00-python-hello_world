3642f737601bca8e773a557b02d33da5d29e7d1b
josephnady <jozephnady#gmail.com>
2024-04-17 07:28:42 -0700

    task15

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,9 +5,8 @@ def weight_average(my_list=[]):
     res = []
     res2 = []
     for x in my_list:
-        # print(f"({x[0]},{x[1]}) = {x[0] * x[1]}")  
-        res.append(x[0] * x[1])  
-        res2.append(x[1])  
+        res.append(x[0] * x[1])
+        res2.append(x[1])
 
     result = sum(res)/sum(res2)
     return (result)
