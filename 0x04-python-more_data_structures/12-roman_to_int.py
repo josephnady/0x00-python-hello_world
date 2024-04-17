2093aea56c5c754f58622724981a4d327f6c0670
joseph nady <jozephnady@gmail.com>
2024-04-17 13:54:36 +0200

    task 13 solution 2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,17 +4,24 @@ def roman_to_int(roman_string):
     X = 10 / VII = 7  / IX = 9 /
     LXXXVII = 87 / DCCVII = 707
     """
-    roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50,
-            "C": 100, "D": 500, "M": 1000}
+    roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    if not isinstance(roman_string, str):
+            return None
+        
+    for each in roman_string:
+        if each  not in list(roman_numeral.keys()):
+            return None        
+
     res = [roman_numeral.get(i) for i in roman_string]
     i = 0
     res2 = []
     for i in res:
         res2.append(i)
-        if i == 10 or 5:
+        if i == 10:
             for x in reversed(res2):
                 if x == 1:
                     res2.remove(x)
                     res2.append(-1)
 
-    return (sum(res2))
+    return sum(res2)
